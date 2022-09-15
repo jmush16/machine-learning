6b0d13733c4172ce893c975553684ad147bf9604
Joel Mushagasha <jmush16@gmail.com>
2022-09-15 17:24:55 -0400

    Added the cookie cutter project structure for project1

---
+++ machine-learning/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the first FCC ML project where I program R-P-S. ',
+    author='Joel Mushagasha',
+    license='MIT',
+)
